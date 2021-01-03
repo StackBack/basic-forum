f76afc2957a315af51c8e954b2004e0214a96223
stackback <vshulenko@gmail.com>
2021-01-03 17:36:09 +0200

    Added Exception Handler

--- src/main/java/com/forum/domain/CustomUser.java
+++ src/main/java/com/forum/domain/CustomUser.java
@@ -3,8 +3,10 @@ package com.forum.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.util.List;
 
@@ -15,8 +17,9 @@ public class CustomUser {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "username", nullable = false)
-    @Size(max = 25)
+    @Size(min = 2,max = 25)
     private String name;
+    @Min(16)
     private Integer age;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
